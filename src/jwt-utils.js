c729cef7bccea198e6b979787ddfc23fcdc10ce9
Orie Steele <orie@ohs.io>
2018-02-05 22:24:28 -0600

    cleanup

--- src/jwt-utils.js
+++ src/jwt-utils.js
@@ -10,8 +10,6 @@ module.exports.create = async (rawClaimsJson, sigKey, encKey) => {
   const token = await JWS.createSign(opt, sigKey)
     .update(payload)
     .final();
-  //   console.log("JWT");
-  //   console.log(token);
 
   // Encrypt JWT
   const encryptedToken = await JWE.createEncrypt(encKey)
@@ -26,8 +24,6 @@ module.exports.verify = async (encodedEncryptedToken, sigKey, encKey) => {
   const decodedEncryptedToken = JSON.parse(
     util.base64url.decode(encodedEncryptedToken, "utf8")
   );
-
-  // console.log("decodedEncryptedToken: ", decodedEncryptedToken);
 
   // Decrypt JWT
   const decryptedToken = await JWE.createDecrypt(encKey).decrypt(
@@ -44,7 +40,7 @@ module.exports.verify = async (encodedEncryptedToken, sigKey, encKey) => {
 
   // Verify Token with jsonwebtoken
   const publicKey = jwkToPem(sigKey.toJSON());
-  const privateKey = jwkToPem(sigKey.toJSON(true), { private: true });
+  // const privateKey = jwkToPem(sigKey.toJSON(true), { private: true });
 
   // console.log("public", publicKey);
   // console.log("private", privateKey);
@@ -70,17 +66,13 @@ module.exports.decodeEthBlockHashJwt = async (web3, jwt) => {
   if (claims.exp < now) {
     throw new Error("jwt expired.");
   }
-
   if (recoveredAddress !== claims.address) {
     throw new Error("recovered address does not match claims.");
   }
-
   const block = await web3.eth.getBlock("latest");
-
   if (block.hash !== claims.blockHash) {
     throw new Error("claims.blockHash is not latest...");
   }
-
   return {
     recoveredAddress,
     claims
